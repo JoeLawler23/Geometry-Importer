bf04165d5a2d036b758dd0668ab6447b044a205d
Joe Lawler <josephlawler23@utexas.edu>
2020-12-29 19:33:01 -0600

    Added an example for unit tests

---
+++ test_example.py
@@ -0,0 +1,16 @@
+import unittest
+
+class Test_Example(unittest.TestCase):
+    def test1(self):
+        """
+        this is an example
+        """
+        pass
+    def test2(self):
+        """
+        this is another example
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
