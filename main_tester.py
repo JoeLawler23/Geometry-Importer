1f5b0cf474b2d887216ff1b59aee6561410dae34
Joe Lawler <josephlawler23@utexas.edu>
2021-06-01 08:41:07 -0500

    Minor Changes

--- main_tester.py
+++ main_tester.py
@@ -3,6 +3,4 @@ import alphabet_to_line
 import geometry_to_line
 
 if __name__ == '__main__':
-    arc = geometry_to_line.circle_to_lines(importer.import_dxf_file('Test Files/Basic Circle.dxf'),3)
-    importer.export_dxf_file('TEST.dxf',arc)
-    importer.export_dxf_file('TEST1.dxf',importer.import_dxf_file('Test Files/Basic Circle.dxf'))
+    arc = importer.import_file('Test Files/Basic Circle.dxf',['CIRCLE'],True)
