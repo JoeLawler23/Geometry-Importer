ec52e3740757b0a86347c9a0d1d4c3b7e364a123
“joseph <josephlawler23@utexas.edu>
2021-06-02 15:27:49 -0500

    Minor

--- main_tester.py
+++ main_tester.py
@@ -1,6 +1,7 @@
+from re import A
 import importer
 import alphabet_to_line
-import geometry_to_line
 
 if __name__ == '__main__':
-    arc = importer.import_file('Test Files/Basic Circle.dxf',['CIRCLE'],True)
+    converted = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,10,0,'mm')
+    importer.export_dxf_file("TEST.dxf", converted, 'mm')
