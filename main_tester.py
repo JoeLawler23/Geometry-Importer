f5cdaec91bb3a461376e9fa424c2dddf32d954db
“joseph <josephlawler23@utexas.edu>
2021-06-05 12:09:49 -0500

    New tests for ellipse to arc

--- main_tester.py
+++ main_tester.py
@@ -3,5 +3,7 @@ import importer
 import alphabet_to_line
 
 if __name__ == '__main__':
-    converted = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,10,0,'mm')
-    importer.export_dxf_file("TEST.dxf", converted, 'mm')
+    converted = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,4,0,'mm')
+    importer.export_dxf_file("TEST3.dxf", converted, 'mm')
+    # converted = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,2,0,'mm')
+    # importer.export_dxf_file("TEST2.dxf", converted, 'mm')
