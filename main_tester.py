f18bfe827eb6df95da2cfdefb93863a4b7b4c66e
Joe Lawler <josephlawler23@utexas.edu>
2021-06-06 16:05:54 -0500

    Unnecessary for stable release

--- main_tester.py
+++
@@ -1,9 +0,0 @@
-from re import A
-import importer
-import alphabet_to_line
-
-if __name__ == '__main__':
-    converted = importer.import_dxf_file('Test Files/Basic Point.dxf')
-    importer.export_dxf_file("TEST3.dxf", converted, 'mm')
-    # converted = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,2,0,'mm')
-    # importer.export_dxf_file("TEST2.dxf", converted, 'mm')
