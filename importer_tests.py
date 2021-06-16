6752673b9e46028613940a6d78c3b35c12339127
JoeLawler23 <josephlawler23@utexas.edu>
2021-06-16 13:40:29 -0500

    Formatting

--- importer_tests.py
+++ importer_tests.py
@@ -17,16 +17,19 @@ class DXF_Error_Tests(unittest.TestCase):
         No file found import_dxf_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.import_dxf_file(""))
+    #end def
     def test_no_extension(self):
         '''
         File missing extension import_dxf_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.export_dxf_file("Test Files/test3",[]))
+    #end def
     def test_no_geometries(self):
         '''
         No passed geometry to export_dxf_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.export_dxf_file("Test Files/test3.dxf",[]))
+    #end def
 
 class DXF_Import_Tests(unittest.TestCase):
     '''
@@ -42,6 +45,7 @@ class DXF_Import_Tests(unittest.TestCase):
         # Check if equal to predetermined values
         point = geometries[0][1][0]  # [0] - First geometry, [1] - Point's values/tuples, [0] - First value/tuple
         self.assertEqual(point,(0.0,0.0,0.0))  # Check point
+    #end def
     def test_complex_points(self):
         '''
         Complex points - multiple points in 3D
@@ -64,6 +68,7 @@ class DXF_Import_Tests(unittest.TestCase):
                 if within_a_percent_tuple(point[0],geometry_values):
                     verified_geometries += 1
         self.assertEqual(verified_geometries, len(points))
+    #end def
     def test_basic_line(self):
         '''
         Basic Line - only one line in 2D
@@ -75,6 +80,7 @@ class DXF_Import_Tests(unittest.TestCase):
         line = geometries[0][1]
         self.assertEqual(line[0],(0.0,0.0,0.0))  # Check start point
         self.assertEqual(line[1],(50.0*10**3,0.0,0.0))  # Check end point
+    #end def
     def test_complex_lines(self):
         '''
         Complex lines - multiple lines in 3D
@@ -110,6 +116,7 @@ class DXF_Import_Tests(unittest.TestCase):
                 if within_a_percent_tuple(line[0],geometry_values[0]) and within_a_percent_tuple(line[1],geometry_values[1]):
                     verified_geometries += 1
         self.assertEqual(verified_geometries, len(lines))
+    #end def
     def test_basic_circles(self):
         '''
         Basic Circle - only one circle in 2D
@@ -121,6 +128,7 @@ class DXF_Import_Tests(unittest.TestCase):
         circle = geometries[0]
         self.assertEqual(circle[1][0],(0.0,0.0,0.0))  # Check center
         self.assertTrue(circle[1][1],(15000.0,0.0,360.0))  # Check radius, start angle, end angle
+    #end def
     def test_complex_circles(self):
         '''
         Complex circles - multiple circles in 3D with various angles and positions
@@ -149,6 +157,7 @@ class DXF_Import_Tests(unittest.TestCase):
                 if within_a_percent_tuple(circle[0],geometry_values[0]) and within_a_percent_tuple(circle[1],geometry_values[1]):
                     verified_geometries += 1
         self.assertEqual(verified_geometries, len(circles))
+    #end def
     def test_basic_arc(self):
         '''
         Basic Arc - only one arc in 2D
@@ -160,6 +169,7 @@ class DXF_Import_Tests(unittest.TestCase):
         arc = geometries[0][1]
         self.assertTrue(arc[0],(0.0,0.0,0.0))  # Check center
         self.assertTrue(within_a_percent_tuple(arc[1],(10.0*10**3,0.0,180.0)))  # Check radius, start angle, end angle
+    #end def
     def test_complex_arcs(self):
         '''
         Complex Arcs - multiple Arcs in 3D
@@ -181,6 +191,7 @@ class DXF_Import_Tests(unittest.TestCase):
                 if within_a_percent_tuple(arc[1],geometry_values[1]) and within_a_percent_tuple(arc[0],geometry_values[0]):
                     verified_geometries += 1
         self.assertEqual(verified_geometries, len(arcs))
+    #end def
     def test_basic_ellipse(self):
         '''
         Basic Ellipse - only one ellipse in 2D
@@ -193,6 +204,7 @@ class DXF_Import_Tests(unittest.TestCase):
         self.assertTrue(ellipse[0],(0.0,0.0,0.0))  # Check center
         self.assertTrue(within_a_percent_tuple(ellipse[1],(25.0*10**3,0.0,0.0)))  # Length of Major Axis
         self.assertTrue(within_a_percent(ellipse[2][0],0.6))  # Ratio of Minor to Major Axis
+    #end def
     def test_complex_ellipsis(self):
         '''
         Complex Ellipsis - multiple ellipses in 3D
@@ -216,6 +228,7 @@ class DXF_Import_Tests(unittest.TestCase):
                 if within_a_percent_tuple(ellipse[0],geometry_values[0]) and within_a_percent_tuple(ellipse[1],geometry_values[1]) and within_a_percent(ellipse[2],geometry_values[2][0]):
                     verified_geometries += 1
         self.assertEqual(verified_geometries, 6)
+    #end def
     def test_basic_spline(self):
         '''
         Basic Spline - only one line in 2D
@@ -230,6 +243,7 @@ class DXF_Import_Tests(unittest.TestCase):
         self.assertTrue(spline[0][2],15)  # # Control Points
         self.assertTrue(spline[0+spline[0][1]],21)  # # Knots
         self.assertTrue(spline[1+spline[0][1]],15)  # # Weights
+    #end def
     def test_basic_lwpolyline(self):
         '''
         Basic LWPolyline - only one line in 2D
@@ -244,6 +258,7 @@ class DXF_Import_Tests(unittest.TestCase):
         self.assertTrue(within_a_percent_tuple(lwpolyline[2],(0.0,25.0*10**3,0.0,0.0,0.0)))  # First point (25,0,0)mm
         self.assertTrue(within_a_percent_tuple(lwpolyline[3],(25.0*10**3,25.0*10**3,0.0,0.0,0.0)))  # First point (25,0,0)mm
         self.assertTrue(lwpolyline[4])  # Closed true
+    #end def
 
 class TXT_Error_Tests(unittest.TestCase):
     '''
@@ -254,11 +269,13 @@ class TXT_Error_Tests(unittest.TestCase):
         No file found import_txt_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.import_txt_file(""))
+    #end def
     def test_no_extension(self):
         '''
         File missing extension import_txt_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.import_txt_file("Test Files/test_geometries"))
+    #end def
 
 class TXT_Import_Tests(unittest.TestCase):
     '''
@@ -270,12 +287,14 @@ class TXT_Import_Tests(unittest.TestCase):
         '''
         geometries = importer.import_txt_file("Test Files/text_2d.txt")
         self.assertEqual(geometries[0][1],(1.1,2.2))
+    #end def
     def test_3d_point(self):
         '''
         One 3D point
         '''
         geometries = importer.import_txt_file("Test Files/text_3d.txt",)
         self.assertEqual(geometries[0][1],(1.1,2.2,3.3))
+    #end def
     def test_various_percision(self):
         '''
         Varying percision of points
@@ -285,12 +304,14 @@ class TXT_Import_Tests(unittest.TestCase):
         self.assertEqual(geometries[1][1],(1.11,2.22,3.33))
         self.assertEqual(geometries[2][1],(1.111,2.222,3.333))
         self.assertEqual(geometries[3][1],(1.1111,2.2222,3.3333))
+    #end def
     def test_no_points(self):
         '''
         No points
         '''
         geometries = importer.import_txt_file("Test Files/text_no_points.txt")
         self.assertEqual(len(geometries),0)
+    #end def
 
 class CSV_Error_Tests(unittest.TestCase):
     '''
@@ -301,11 +322,13 @@ class CSV_Error_Tests(unittest.TestCase):
         No file found import_csv_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.import_csv_file(""))
+    #end def
     def test_no_extension(self):
         '''
         File missing extension import_csv_file throws error
         '''
         self.assertRaises(Exception, lambda: importer.import_csv_file("Test Files/test_geometries"))
+    #end def
 
 class CSV_Import_Tests(unittest.TestCase):
     '''
@@ -333,17 +356,18 @@ class CSV_Import_Tests(unittest.TestCase):
         self.assertEqual(geometries[3][1][0],(1.0,2.0,3.0))
         self.assertEqual(geometries[3][1][1],(4.0,0.0,0.0))
         self.assertEqual(geometries[3][1][2],(5.0,))
+    #end def
 
 class Conversion_Tests(unittest.TestCase):
     '''
     Tests for converting geometries
     '''
     def test_line_to_point(self):
-        """
+        '''
         Test converting lines to points using:
             - number of segments
             - segment length
-        """
+        '''
 
         # Down-convert line to 10 points
         converted_numsegments = importer.import_dxf_file('Test Files/Basic Line.dxf',['POINT'],True,10)
@@ -364,13 +388,13 @@ class Conversion_Tests(unittest.TestCase):
         # Check spacing is good
         for index2,point2 in enumerate(converted_segmentlength):
             self.assertEqual((index2*1000,0.0,0.0),point2[1][0])
-        
+    #end def 
     def test_arc_to_line(self):
-        """
+        '''
         Test converting arcs to lines using:
             - number of segments
             - segment length
-        """
+        '''
 
         # Down-convert arc to 10 lines
         converted_numsegments = importer.import_dxf_file('Test Files/Basic Arc.dxf',['LINE'],True,10)
@@ -385,31 +409,31 @@ class Conversion_Tests(unittest.TestCase):
         for index in range(0,len(converted_segmentlength)-1):
             line = converted_segmentlength[index]
             self.assertTrue(within_a_percent(round(math.dist(line[1][0],line[1][1]),3),1000))
-    
+    #end def
     def test_ellipse_to_arc(self):
-        """
+        '''
         Test converting ellipse to arcs using:
             - number of segments
-        """
+        '''
 
         # Down-convert ellipse to 10 arcs
         converted_numsegments = importer.import_dxf_file('Test Files/Basic Ellipse.dxf',['ARC'],True,10)
 
         # Check if 10 arcs were created
         self.assertEqual(len(converted_numsegments), 10)
-    
+    #end def
     def test_spline_to_line(self):
-        """
+        '''
         Basic test to make sure down converting splines to lines function throws no error
-        """
+        '''
 
         # Down-convert spline to lines
         converted_numsegments = importer.import_dxf_file('Test Files/Basic Spline.dxf',['LINE'],True)
-    
+    #end def
     def test_lwpoline_to_line(self):
-        """
+        '''
         Manually check that Basic LWPolyline gets converted into the predefined lines
-        """
+        '''
 
         # Down-convert lwpolyline into lines
         converted_numsegments = importer.import_dxf_file('Test Files/Basic LWPolyline.dxf',['LINE'],True)
@@ -423,6 +447,7 @@ class Conversion_Tests(unittest.TestCase):
         self.assertTrue(within_a_percent_tuple(converted_numsegments[2][1][1],(25000.0,25000.0,0.0)))
         self.assertTrue(within_a_percent_tuple(converted_numsegments[3][1][0],(25000.0,25000.0,0.0)))
         self.assertTrue(within_a_percent_tuple(converted_numsegments[3][1][1],(25000.0,0.0,0.0)))
+    #end def
 
 # Verification functions
 
@@ -443,6 +468,7 @@ def within_a_percent_tuple(tuple1: tuple[float,...], tuple2: tuple[float,...]) -
         if not within_a_percent(float1,float2):
             return False
     return True
+#end def
 
 def within_a_percent(float1: float, float2: float) -> bool:
     '''
@@ -471,3 +497,4 @@ def within_a_percent(float1: float, float2: float) -> bool:
          return False
 
     return True
+#end def
